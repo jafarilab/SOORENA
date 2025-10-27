3621ca88f2eece33730c3ff11abd67dfb17497a4
Hala Arar <halaosama92@yahoo.com>
2025-10-27 18:15:48 -0400

    delete extra

--- shiny_app/test_load.R
+++
@@ -1,7 +0,0 @@
-# Test data loading
-df <- read.csv("data/predictions_for_app.csv", stringsAsFactors = FALSE)
-print(paste("Rows:", nrow(df)))
-print("Columns:")
-print(colnames(df))
-print("\nSample:")
-print(head(df, 2))
